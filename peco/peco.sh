ffb2382fa97a52de0b096adca73ca22c2eac42fb
Mathias Söderberg <mathias.soederberg@gmail.com>
2015-08-29 16:03:16 +0200

    peco: update history widget

--- peco/peco.sh
+++ peco/peco.sh
@@ -2,6 +2,7 @@ peco-history-widget() {
   LBUFFER=$(fc -lr 1 | peco | sed "s/ *[0-9*]* *//")
   zle redisplay
 }
+
 zle -N peco-history-widget
-bindkey '^x^r' peco-history-widget
 bindkey '^xr' peco-history-widget
+bindkey '^x^r' peco-history-widget
