5e888223316fdb80c3049e2a4b819e4ce0c5b10d
Mathias Söderberg <mths@sdrbrg.se>
2017-04-09 22:11:48 +0100

    zsh: revamp + move setup to tag

---
+++ tag-zsh/config/zsh/functions.zsh
@@ -0,0 +1,47 @@
+function jo() {
+  env JAVA_OPTS="-Xmn512m -Xms2g -Xmx2g" "$@"
+}
+
+function flip() {
+  perl -C3 -Mutf8 -lpe '$_=reverse;y/a-zA-Z.['\'',({?!\"<_;‿⁅∴\r/ɐqɔpǝɟƃɥıɾʞ|ɯuodbɹsʇnʌʍxʎzɐqɔpǝɟƃɥıɾʞ|ɯuodbɹsʇnʌʍxʎz˙],'\'')}¿¡,>‾؛⁀⁆∵\n/' <<< "$1"
+}
+
+function fuck() {
+  CMD="pkill"
+  which $CMD >/dev/null || CMD="killall"
+  echo
+  if [ "$1" "==" "off" ]; then
+    shift
+    FLIP=(' (ノಠ益ಠ)ノ彡' '(ノಠ-ಠ)ノ彡')
+    SIG="-9"
+  else
+    [ "$1" "==" "you" ] && shift
+    FLIP=(' (╯°□°）╯︵' '(ノ ゜Д゜)ノ ︵' '(ﾉಥДಥ)ﾉ︵' ' ヽ(`Д´)ﾉ︵' ' (ﾉಥ益ಥ）ﾉ ')
+    SIG=""
+  fi
+  [ -z "$1" ] && { echo "┬─┬﻿ ノ( ゜-゜ノ) patience young grasshopper"; echo; return; }
+  F=${FLIP[$RANDOM % ${#FLIP[@]} ]}
+  if $CMD $SIG "$1"; then
+    echo "$F$(flip $1)"; echo
+  else
+    echo "┬─┬﻿ ︵ /(.□. \）"; echo
+  fi
+}
+
+function gt() {
+  cd $(git rev-parse --show-cdup);
+}
+
+function c() {
+  local repo
+  repo="$(ghq list | peco --select-1)"
+  [[ -n "$repo" ]] && cd "$DEV_SRC/$repo"
+}
+
+function m() {
+  if (( $# == 0 )); then
+    $EDITOR .
+  else
+    $EDITOR "$@"
+  fi
+}
